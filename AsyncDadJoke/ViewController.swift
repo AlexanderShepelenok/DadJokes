7c413bf09195d717baed4e30ce3aac71a8ce4562
Aleksandr Shepelenok <aleksandr.shepelenok@itechart-group.com>
2022-03-17 12:50:28 +0500

    Initial Commit

---
+++ AsyncDadJoke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsyncDadJoke
+//
+//  Created by Aleksandr on 17/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
